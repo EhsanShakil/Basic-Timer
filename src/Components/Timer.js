08d72e9a87b1de6067844ac8c6ab3bf0e674b915
EhsanShakil <ahsanshakeel1994@gmail.com>
2020-12-11 23:31:40 +0500

    trying to add component on bit.dev

---
+++ src/Components/Timer.js
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import "./Timer.css";
+
+function Timer() {
+  const [hour, setHour] = useState();
+  const [minute, setMinute] = useState();
+  const [second, setSecond] = useState();
+
+  const timer = () => {
+    const date = new Date();
+    setHour(date.getHours());
+    setMinute(date.getMinutes());
+    setSecond(date.getSeconds());
+  };
+  setInterval(timer, 1000);
+
+  return (
+    <div className="App">
+      <div className="timer">
+        <h1>Hours</h1>
+        <p className="clock">{hour}</p>
+      </div>
+      <div className="timer">
+        <h1>Minutes</h1>
+        <p className="clock">{minute}</p>
+      </div>
+      <div className="timer">
+        <h1>Seconds</h1>
+        <p className="clock">{second}</p>
+      </div>
+    </div>
+  );
+}
+
+export default Timer;
